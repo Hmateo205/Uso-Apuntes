1fe16e84b8be640c56cc375c3dac876c69bd2c7a
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-23 10:30:42 -0500

    Restabecer Main page

--- Uso Apuntes/Paginas/Informacion.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Uso_Apuntes.Paginas;
-
-public partial class Informacion : ContentPage
-{
-	public Informacion()
-	{
-		InitializeComponent();
-	}
-}
--- Uso Apuntes/Paginas/Notas.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Uso_Apuntes.Paginas;
-
-public partial class Notas : ContentPage
-{
-	public Notas()
-	{
-		InitializeComponent();
-	}
-}
