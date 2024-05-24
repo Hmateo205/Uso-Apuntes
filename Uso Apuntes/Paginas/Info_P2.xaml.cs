654a79aed70cc0a63e255ba0f90d1310b4a52a9f
Hernan Mateo Jurado Moran <hernan.jurado@udla.edu.ec>
2024-05-24 17:42:17 -0500

    Creacion Xaml Paginas

---
+++ Uso Apuntes/Paginas/Info_P2.xaml.cs
@@ -0,0 +1,9 @@
+namespace Uso_Apuntes.Paginas;
+
+public partial class Info_P2 : ContentPage
+{
+	public Info_P2()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Uso Apuntes/Paginas/Notas_P1.xaml.cs
@@ -0,0 +1,9 @@
+namespace Uso_Apuntes.Paginas;
+
+public partial class Notas_P1 : ContentPage
+{
+	public Notas_P1()
+	{
+		InitializeComponent();
+	}
+}
